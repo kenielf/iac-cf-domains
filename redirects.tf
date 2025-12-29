5bd1c8ce067438914fab1d2b3808c4a8d10492af
kenielf <chrystian.keni.github@gmail.com>
2025-12-29 07:05:06 -0300

    fix(redirects): wrong schema

--- redirects.tf
+++ redirects.tf
@@ -1,31 +1,30 @@
 resource "cloudflare_ruleset" "redirects" {
   depends_on = [ cloudflare_dns_record.this ]
-  for_each = { for k, v in var.records: k => v if v.redirect != null }
+  for_each   = { for k, v in var.records : k => v if v.redirect != null }
 
   zone_id = var.zone_id
   name    = format("%s: %s (Redirect)", each.value.project, each.key)
   kind    = "zone"
   phase   = "http_request_dynamic_redirect"
 
-  rules {
-    description = format("Redirect %s.%s to %s", each.value.subdomain, var.domain, each.value.redirect)
-    enabled     = true
+  rules = [
+    {
+      description = format("Redirect %s.%s to %s", each.value.subdomain, var.domain, each.value.redirect)
 
-    expression = <<-EOF
-      (http.host eq "${each.value.subdomain}.${var.domain}")
-    EOF
+      enabled = true
+      expression = format("(http.host eq \"%s.%s\")", each.value.subdomain, var.domain)
+      action = "redirect"
 
-    action = "redirect"
-
-    action_parameters {
-      from_value {
-        status_code = 301
-        target_url {
-          value = each.value.redirect
+      action_parameters = {
+        from_value = {
+          status_code = 301
+          target_url = {
+            value = each.value.redirect
+          }
+          preserve_query_string = true
         }
-        preserve_query_string = true
       }
     }
-  }
+  ]
 }
 
